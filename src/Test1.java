9d58c32c8f3db9ca5ce10642a77516065fccba08
Lijin <li1599185635@126.com>
2023-02-28 15:26:00 +0800

    dev分支的第一次提交

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello baizhi!");
         System.out.println("版本2");
+        System.out.println("dev分支上的修改");
     }
 }
